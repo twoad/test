2e0a6feee53d561db44d007d9fa7c8cbf331ddae
twoad <102876980+twoad@users.noreply.github.com>
2022-04-02 16:50:24 -0400

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -12,7 +12,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/twoad/test/04c82bbb87cc7163a29c41b2ccb4f9c9fec33786/kaguya-overlay.png";
+            i.src = "https://raw.githubusercontent.com/twoad/test/8de9ad6c4a2960f2980b36bbbba977179416aac7/kaguya-overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
